c0519059d591c75b746d64c00e74d14cf1385397
a8568730 <a8568730@hotmail.com>
2017-04-24 17:15:47 +0800

    新增demo/index.jsx

---
+++ demo/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Layout } from '../lib';
+import '../demo-ui.css';
+
+console.log('khiau2');
+
+render(
+  <Layout>Hello demo-ui</Layout>,
+  document.getElementById('app')
+);
