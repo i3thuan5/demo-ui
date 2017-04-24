9e728fce1228887f5eeae4d9b73527d088af179f
a8568730 <a8568730@hotmail.com>
2017-04-24 17:18:49 +0800

    webpack進入點為demo/index.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Layout = ({ children }) => (
   <div className='app background'>
-  {this.props.children}
+  {children}
   </div>
 );
 
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,10 +5,10 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
-    './src/index',
+    './demo/index',
   ],
   output: {
-    path: path.join(__dirname, 'dev'),
+    path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/',
   },
@@ -23,11 +23,10 @@ module.exports = {
     loaders: [{
       test: /\.jsx?/,
       loaders: ['babel', 'strict'],
-      include: path.join(__dirname, 'src'),
     },
     {
       test: /\.css$/,
-      loader: 'style-loader!css-loader!postcss-loader',
+      loader: 'style-loader!css-loader',
     },
     ],
   },
