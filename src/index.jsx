3c28047bd0a9385be00e73b257c2f0bb530e1f97
a8568730 <a8568730@hotmail.com>
2017-04-25 16:33:20 +0800

    改從semantic-ui-css匯入需要的元素css

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
-// import 'semantic-ui-container/container.min.css';
-// import 'semantic-ui-menu/menu.min.css';
-// import 'semantic-ui-button/button.min.css';
+import 'semantic-ui-css/components/reset.min.css';
+import 'semantic-ui-css/components/container.min.css';
+import 'semantic-ui-css/components/menu.min.css';
+import 'semantic-ui-css/components/button.min.css';
 import '../demo-ui.css';
 
 export {default as Layout} from './Layout';
