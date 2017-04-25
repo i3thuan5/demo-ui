fe9ffe5c1b543319cce4d11245984ed7a56446f2
a8568730 <a8568730@hotmail.com>
2017-04-25 13:54:17 +0800

    demo加上footer

--- demo/index.jsx
+++ demo/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Layout } from '../lib';
+import { Layout, Footer } from '../lib';
 
-console.log('khiau2');
+const sites = [{
+      title: '意傳文化科技',
+      url: 'https://xn--v0qr21b.xn--kpry57d/',
+    }, ];
 
 render(
-  <Layout>Hello demo-ui</Layout>,
+  <Layout>
+    <h1>Hello demo-ui</h1>
+    <Footer sites={sites}/>
+  </Layout>,
   document.getElementById('app')
 );
