8a14122a69213020f2546f12a68242fbe2eec8b1
a8568730 <a8568730@hotmail.com>
2017-04-25 17:30:59 +0800

    匯入semantic-ui-css/form.min.css

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import 'semantic-ui-css/components/reset.min.css';
 import 'semantic-ui-css/components/container.min.css';
 import 'semantic-ui-css/components/menu.min.css';
 import 'semantic-ui-css/components/button.min.css';
+import 'semantic-ui-css/components/form.min.css';
 import '../demo-ui.css';
 
 export {default as Layout} from './Layout';
