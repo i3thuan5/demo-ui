da57f007b2de9d4aa6e83d07303d38bcfad3225c
a8568730 <a8568730@hotmail.com>
2017-04-28 19:27:33 +0800

    demo-ui.css 移動到asset/css

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import 'semantic-ui-css/components/container.min.css';
 import 'semantic-ui-css/components/menu.min.css';
 import 'semantic-ui-css/components/button.min.css';
 import 'semantic-ui-css/components/form.min.css';
-import '../demo-ui.css';
+import '../asset/css/demo-ui.css';
 
 export {default as Layout} from './Layout';
 export {default as Footer} from './Footer';
