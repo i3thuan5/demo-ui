dde376bbc707e40b5a0864b4c68dd1cc681d171f
a8568730 <a8568730@hotmail.com>
2017-05-10 11:23:36 +0800

    指定audio.node.load，蓋掉jsdom的HTMLMediaElemt

--- test/component/PlayButton.spec.js
+++ test/component/PlayButton.spec.js
@@ -1,3 +1,4 @@
+import 'jsdom-global/register';
 import React from "react";
 import { expect } from "chai";
 import { shallow, mount } from "enzyme";
@@ -12,12 +13,18 @@ const shallowSetup = (語句 = "逐家") => {
 
 const mountSetup = (語句 = "逐家") => {
   return mount(
-    <PlayButton 語句={語句}/>
+    <PlayButton 語句={語句}/>,
+    { attachTo: document.body.firstChild }
   );
 };
 
 describe("Component", () => {
   describe("PlayButton", () => {
+    
+    beforeEach(function() {
+      document.body.innerHTML = '<div></div>';
+    });
+
     it("renders audio", () => {
       const wrapper = shallowSetup();
       expect(wrapper.find("audio")).to.have.length(1);
@@ -26,22 +33,21 @@ describe("Component", () => {
       const wrapper = shallowSetup();
       expect(wrapper.find("button")).to.have.length(1);
     });
-    // it("updates audio ref", () => {
-    //   const wrapper = mount(<PlayButton 語句={"逐家"}/>);
-    //   // const wrapper = mountSetup();
-    //   // wrapper.setProps({ 語句: '語句' });
-    //   // expect(componentWillReceiveProps.calledOnce).to.equal(true);
-    // });
-
-    // it("ignores same src", () => {
-    //   const { wrapper } = mountSetup();
-    //   wrapper.setProps({ 語句: '逐家' });
-    //   expect(componentWillReceiveProps.calledOnce)
-    //   .to.equal(false);
-    // });
-    // it("call play on click", () => {
-    //   const { button } = setup();
-    //   expect(audio).to.have.length(1);
-    // });
+    it("reloads audio when update props", () => {
+      const wrapper = mountSetup();
+      let sui='0';
+      let audio = wrapper.ref('合成音檔');
+      audio.node.load = () => {sui='1'};
+      wrapper.setProps({ 語句: '語句' });
+      expect(sui).to.equal('1');
+    });
+    it("ignores same props", () => {
+      const wrapper = mountSetup();
+      let sui='0';
+      let audio = wrapper.ref('合成音檔');
+      audio.node.load = () => {sui='1'};
+      wrapper.setProps({ 語句: '逐家' });
+      expect(sui).to.equal('0');
+    });
   });
 });
