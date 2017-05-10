1dbb0209f98e687e44f8ad925413577e3255424e
a8568730 <a8568730@hotmail.com>
2017-05-10 14:13:42 +0800

    複製鈕和spec

---
+++ src/component/CopyButton.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "../../asset/icon-font/fontello.min.css";
+
+class CopyButton extends React.Component {
+
+  copy() {
+    const { 複製內容 } = this.props;
+    const textField = document.createElement("textarea");
+    textField.innerText = 複製內容;
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand("copy");
+    textField.remove();
+  }
+
+  render() {
+    const { 按鈕名 } = this.props;
+    return (
+      <button onClick={this.copy.bind(this)}
+        className='ui icon button'>
+        <i className='icon-docs'></i>
+        {按鈕名}
+      </button>
+    );
+  }
+}
+
+
+CopyButton.propTypes = {
+  複製內容: PropTypes.string.isRequired,
+};
+
+export default CopyButton;
---
+++ test/component/CopyButton.spec.js
@@ -0,0 +1,37 @@
+import 'jsdom-global/register';
+import React from "react";
+import { expect } from "chai";
+import { shallow, mount } from "enzyme";
+import sinon from 'sinon';
+import { CopyButton } from "../../lib";
+
+const shallowSetup = (內容 = "請複製這句話") => 
+  shallow(<CopyButton 複製內容={內容}/>)
+
+const mountSetup = (內容 = "請複製這句話") => {
+  const wrapper = mount(
+    <CopyButton 複製內容={內容}/>,
+    { attachTo: document.body.firstChild }
+  );
+  const button = wrapper.find('button'); 
+  //mock document copy method
+  document.execCommand = function(){return true;}
+  const oncopy = sinon.spy(document, 'execCommand');
+  return { 
+    wrapper, button, oncopy
+  }
+};
+
+describe("Component", () => {
+  describe("CopyButton", () => {   
+    it("renders button", () => {
+      const wrapper = shallowSetup();
+      expect(wrapper.find("button")).to.have.length(1);
+    });
+    it("copys on click", () => {
+      const { button, oncopy } = mountSetup();
+      button.simulate('click');
+      expect(oncopy.called).to.equal(true);
+    });
+  });
+});
