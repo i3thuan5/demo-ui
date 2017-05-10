bddb26d31d0a65ed9dfc1aad199b2330606f0c73
a8568730 <a8568730@hotmail.com>
2017-05-10 11:46:07 +0800

    測試點擊後有呼叫audio.play

--- test/component/PlayButton.spec.js
+++ test/component/PlayButton.spec.js
@@ -16,10 +16,13 @@ const mountSetup = (語句 = "逐家") => {
     <PlayButton 語句={語句}/>,
     { attachTo: document.body.firstChild }
   );
-  let audio = wrapper.ref('合成音檔');
-  let onload = sinon.stub(audio.node, "load");
+  const audio = wrapper.ref('合成音檔');
+  const onload = sinon.stub(audio.node, "load");
+  const onplay = sinon.stub(audio.node, "play");
+  const button = wrapper.find('button'); 
   return { 
-    wrapper, audio, onload 
+    wrapper, audio, onload, onplay,
+    button
   }
 };
 
@@ -44,9 +47,15 @@ describe("Component", () => {
       expect(onload.calledOnce).to.equal(true);
     });
     it("ignores same props", () => {
-      const { wrapper, audio, onload } = mountSetup('逐家');
+      const { wrapper, onload } = mountSetup('逐家');
       wrapper.setProps({ 語句: '逐家' });
       expect(onload.called).to.equal(false);
+    });
+    it("plays audio on click", () => {
+      const { wrapper, onplay, button } = mountSetup('逐家');
+      wrapper.setProps({ 語句: '語句' });
+      button.simulate('click');
+      expect(onplay.called).to.equal(true);
     });
   });
 });
