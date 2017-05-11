dff9a82c44a2178a171ee9f3a631d1c0f8b35c10
a8568730 <a8568730@hotmail.com>
2017-05-11 10:09:58 +0800

    prop漢字詞改成漢字

--- src/component/HanLoSu.jsx
+++ src/component/HanLoSu.jsx
@@ -3,18 +3,18 @@ import PropTypes from 'prop-types';
 
 class HanLoSu extends React.Component {
   render() {
-    const { 臺羅閏號調, 漢字詞 } = this.props;
+    const { 臺羅閏號調, 漢字 } = this.props;
     return (
       <ruby className="app ruby">
       {臺羅閏號調}
-      <rt>{漢字詞}</rt>
+      <rt>{漢字}</rt>
       </ruby>
     );
   }
 }
 
 HanLoSu.propTypes = {
-  漢字詞: PropTypes.string.isRequired,
+  漢字: PropTypes.string.isRequired,
   臺羅閏號調: PropTypes.string.isRequired,
 };
 
