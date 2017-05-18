e610e75d5e9031672df1205cba9f1b1b11975d76
a8568730 <a8568730@hotmail.com>
2017-05-18 18:51:36 +0800

    檢查分詞格式。空字串和全形｜是合格的。

--- src/vendors/CustomPropTypes.js
+++ src/vendors/CustomPropTypes.js
@@ -1,9 +1,11 @@
 export const 分詞格式 = (props, propName, componentName) => {
-  if (!/｜/.test(props[propName])) {
+  if (props[propName] !== '' && 
+    !/｜/.test(props[propName])) 
+  {
     return new Error(
       'Invalid prop `' + propName + '` supplied to' +
       ' `' + componentName + '`. Should follow 分詞 format.'+
       ' Validation failed.'
     );
   }
-}
+};
