8106c20d7de2243bab90ba46738741942e5df3c6
a8568730 <a8568730@hotmail.com>
2017-05-19 16:45:23 +0800

    播放中的顏色

--- src/component/PlayButton.jsx
+++ src/component/PlayButton.jsx
@@ -5,11 +5,19 @@ import {分詞格式} from '../vendors/CustomPropTypes';
 import "../../asset/icon-font/fontello.min.css";
 
 class PlayButton extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPlaying: false
+    };
+  }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.分詞 === this.props.分詞) return;
     const 音檔 = this.refs.合成音檔;
     音檔.load();
+    this.setState({isPlaying:false});
   }
 
   play() {
@@ -17,16 +25,27 @@ class PlayButton extends React.Component {
     音檔.play();
   }
 
+  handlePlay() {
+    this.setState({isPlaying:true});
+  }
+
+  handleEnd() {
+    this.setState({isPlaying:false});
+  }
+
   render() {
     const { 分詞, 腔口 } = this.props;
     const src = API.語音合成({分詞, 腔口});
     return (
         <span>
-          <audio ref="合成音檔">
+          <audio ref="合成音檔"
+          onPlay={this.handlePlay.bind(this)}
+          onEnded={this.handleEnd.bind(this)}>
             <source type="audio/wav" src={src}/>
           </audio>
           <button onClick={this.play.bind(this)}
-            className='ui compact icon massive button'>
+            className={`ui compact icon massive button 
+              ${this.state.isPlaying ? ' grey': ''}`}>
             <i className='icon-play'></i>
           </button>
         </span>
