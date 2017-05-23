092ec9c8e86c6d84bd617e65dfeea3196fac13e4
a8568730 <a8568730@hotmail.com>
2017-05-23 17:07:31 +0800

    意傳服務新增文本直接合成。

--- test/api/意傳服務.api.spec.js
+++ test/api/意傳服務.api.spec.js
@@ -9,7 +9,7 @@ describe("服務.意傳.台灣", () => {
       const domain = API.網域();
       expect(domain).to.equal(initDomain);
     });
-    it("returns 語音合成", () => {
+    it("returns 語音合成 閩南語", () => {
       const url = API.語音合成({
         腔口: "閩南語",
         分詞: "逐-家｜Tak8-ke1"
@@ -27,6 +27,16 @@ describe("服務.意傳.台灣", () => {
       expect(url).to.equal(encodeURI(
         initDomain + 
         "語音合成?查詢腔口=四縣腔&查詢語句=逐-家｜Tak8-ke1"
+      ));
+    });
+    it("returns 文本直接合成 Pangcah", () => {
+      const url = API.文本直接合成({
+        腔口: "Pangcah",
+        語句: "Maolahay"
+      });
+      expect(url).to.equal(encodeURI(
+        initDomain + 
+        "文本直接合成?查詢腔口=Pangcah&查詢語句=Maolahay"
       ));
     });
   });
